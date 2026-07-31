4b93d280a954121c4263c17091ae0378d208bf1b
Felicia Limanta <siplusplus2019@gmail.com>
2026-07-31 22:47:24 +0700

    feat: articles subscription (#4)
    * feat: add custom styled email template for article notifications
    
    * ci: add github action to trigger article email notifications via appscript
    
    * feat: add subscribe button to website footer

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,5 @@ export const SITE_NAME = 'Felicia Limanta';
 export const SITE_BIO = 'A lifelong learner growing with the community. Obsessed with improving DXs and UXs. Likes to write occasionally.';
 export const SITE_DESCRIPTION = 'Thoughts on life, volunteering, and everything in between.';
 export const SITE_FOOTER = 'Built with [ SvelteKit, Vite ] by [ Felicia Limanta, Antigravity ]';
+export const SUBSCRIBE_FORM_URL = 'https://forms.gle/meMn9YwvaPrQi8an6';
+
